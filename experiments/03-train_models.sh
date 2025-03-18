a63c577272442f8a1b8c0c2b92ddb18360220bb0
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-03-18 07:29:42 +0100

    launch more

--- experiments/03-train_models.sh
+++ experiments/03-train_models.sh
@@ -5,12 +5,12 @@ sbatch_gpu_bigg "train_multicand_1t00s" "comet-multi-cand-train --cfg comet_mult
 sbatch_gpu_bigg "train_multicand_1t01s" "comet-multi-cand-train --cfg comet_multi_cand/configs/experimental/model_multicand_1t01s.yaml"
 sbatch_gpu_bigg "train_multicand_1t10s" "comet-multi-cand-train --cfg comet_multi_cand/configs/experimental/model_multicand_1t10s.yaml"
 # two additional translations
-sbatch_gpu_bigg "train_multicand_2t00s" "comet-multi-cand-train --cfg comet_multi_cand/configs/experimental/model_multicand_2t00s.yaml" # in training
+sbatch_gpu_bigg "train_multicand_2t00s" "comet-multi-cand-train --cfg comet_multi_cand/configs/experimental/model_multicand_2t00s.yaml"
 sbatch_gpu_bigg "train_multicand_2t20s" "comet-multi-cand-train --cfg comet_multi_cand/configs/experimental/model_multicand_2t20s.yaml"
 sbatch_gpu_bigg "train_multicand_2t02s" "comet-multi-cand-train --cfg comet_multi_cand/configs/experimental/model_multicand_2t02s.yaml"
 # three additional translations
 sbatch_gpu_bigg "train_multicand_3t00s" "comet-multi-cand-train --cfg comet_multi_cand/configs/experimental/model_multicand_3t00s.yaml" # in training
-sbatch_gpu_bigg "train_multicand_3t30s" "comet-multi-cand-train --cfg comet_multi_cand/configs/experimental/model_multicand_3t30s.yaml" # in training
+sbatch_gpu_bigg "train_multicand_3t30s" "comet-multi-cand-train --cfg comet_multi_cand/configs/experimental/model_multicand_3t30s.yaml" # in training, failed
 sbatch_gpu_bigg "train_multicand_3t03s" "comet-multi-cand-train --cfg comet_multi_cand/configs/experimental/model_multicand_3t03s.yaml" # in training
 # four additional translations
 sbatch_gpu_bigg "train_multicand_4t00s" "comet-multi-cand-train --cfg comet_multi_cand/configs/experimental/model_multicand_4t00s.yaml" # todo training
--- experiments/10-eval_models.sh
+++ experiments/10-eval_models.sh
@@ -1,14 +1,14 @@
 # pairwise (via da)
-sbatch_gpu_short "eval_pw_0t00s_da" "python3 experiments/11-eval_pw_test_da.py lightning_logs/multicand_0t00s/checkpoints/epoch\=4*" # running
-sbatch_gpu_short "eval_pw_1t00s_da" "python3 experiments/11-eval_pw_test_da.py lightning_logs/multicand_1t00s/checkpoints/epoch\=4*" # running
-sbatch_gpu_short "eval_pw_1t10s_da" "python3 experiments/11-eval_pw_test_da.py lightning_logs/multicand_1t10s/checkpoints/epoch\=4*" # running
-sbatch_gpu_short "eval_pw_1t01s_da" "python3 experiments/11-eval_pw_test_da.py lightning_logs/multicand_1t01s/checkpoints/epoch\=4*" # running
+sbatch_gpu_short "eval_pw_0t00s_da" "python3 experiments/11-eval_pw_test_da.py lightning_logs/multicand_0t00s/checkpoints/epoch\=4*"
+sbatch_gpu_short "eval_pw_1t00s_da" "python3 experiments/11-eval_pw_test_da.py lightning_logs/multicand_1t00s/checkpoints/epoch\=4*"
+sbatch_gpu_short "eval_pw_1t10s_da" "python3 experiments/11-eval_pw_test_da.py lightning_logs/multicand_1t10s/checkpoints/epoch\=4*"
+sbatch_gpu_short "eval_pw_1t01s_da" "python3 experiments/11-eval_pw_test_da.py lightning_logs/multicand_1t01s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_pw_2t02s_da" "python3 experiments/11-eval_pw_test_da.py lightning_logs/multicand_2t02s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_pw_2t20s_da" "python3 experiments/11-eval_pw_test_da.py lightning_logs/multicand_2t20s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_pw_2t00s_da" "python3 experiments/11-eval_pw_test_da.py lightning_logs/multicand_2t00s/checkpoints/epoch\=4*"
 
 # pairwise (direct)
-sbatch_gpu_short "eval_pw_1t01s_pw" "python3 experiments/12-eval_pw_test_pw.py lightning_logs/multicand_1t01s/checkpoints/epoch\=4*" # running
+sbatch_gpu_short "eval_pw_1t01s_pw" "python3 experiments/12-eval_pw_test_pw.py lightning_logs/multicand_1t01s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_pw_2t02s_pw" "python3 experiments/12-eval_pw_test_pw.py lightning_logs/multicand_2t02s/checkpoints/epoch\=4*"
 
 # da (random)
@@ -18,10 +18,20 @@ sbatch_gpu_short "eval_da_1t10s_da" "python3 experiments/13-eval_da_test.py ligh
 sbatch_gpu_short "eval_da_1t01s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_1t01s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_da_2t02s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_2t02s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_da_2t20s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_2t20s/checkpoints/epoch\=4*"
-sbatch_gpu_short "eval_da_2t00s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_2t00s/checkpoints/epoch\=4*"
+sbatch_gpu_short "eval_da_2t00s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_2t00s/checkpoints/epoch\=4*" # running
 
 # da (closest)
 sbatch_gpu_short "eval_da_0t00s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_0t00s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_da_1t00s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_1t00s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_da_1t10s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_1t10s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_da_1t01s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_1t01s/checkpoints/epoch\=4*"
+
+# reference-based
+sbatch_gpu_short "eval_da_ref_0t00s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_ref_0t00s/checkpoints/epoch\=4*" # in training
+sbatch_gpu_short "eval_da_ref_1t00s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_ref_1t00s/checkpoints/epoch\=4*" # in training
+sbatch_gpu_short "eval_da_ref_1t10s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_ref_1t10s/checkpoints/epoch\=4*" # in training
+sbatch_gpu_short "eval_da_ref_1t01s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_ref_1t01s/checkpoints/epoch\=4*" # in training
+sbatch_gpu_short "eval_da_ref_0t00s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_ref_0t00s/checkpoints/epoch\=4*"
+sbatch_gpu_short "eval_da_ref_1t00s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_ref_1t00s/checkpoints/epoch\=4*"
+sbatch_gpu_short "eval_da_ref_1t10s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_ref_1t10s/checkpoints/epoch\=4*"
+sbatch_gpu_short "eval_da_ref_1t01s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_ref_1t01s/checkpoints/epoch\=4*"
