c53d4fb4664b61beffd8bd2d38d714651d018d85
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-05-20 18:41:35 +0200

    more generic evaluator

--- experiments/10-eval_models.sh
+++ experiments/10-eval_models.sh
@@ -23,16 +23,20 @@ sbatch_gpu_short "eval_da_3t00s_da" "python3 experiments/13-eval_da_test.py ligh
 sbatch_gpu_short "eval_da_3t30s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_3t30s/checkpoints/epoch\=4*"
 
 # da (closest)
-sbatch_gpu_short "eval_da_0t00s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_0t00s/checkpoints/epoch\=4*"
-sbatch_gpu_short "eval_da_1t00s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_1t00s/checkpoints/epoch\=4*"
-sbatch_gpu_short "eval_da_1t10s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_1t10s/checkpoints/epoch\=4*"
-sbatch_gpu_short "eval_da_1t01s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_1t01s/checkpoints/epoch\=4*"
+sbatch_gpu_short "eval_da_0t00s_da_sim" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_0t00s/checkpoints/epoch\=4* --data data/csv/test_multi_sim.csv"
+sbatch_gpu_short "eval_da_1t00s_da_sim" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_1t00s/checkpoints/epoch\=4* --data data/csv/test_multi_sim.csv"
+sbatch_gpu_short "eval_da_1t10s_da_sim" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_1t10s/checkpoints/epoch\=4* --data data/csv/test_multi_sim.csv"
+sbatch_gpu_short "eval_da_1t01s_da_sim" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_1t01s/checkpoints/epoch\=4* --data data/csv/test_multi_sim.csv"
 
 # reference-based
 sbatch_gpu_short "eval_da_ref_0t00s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_ref_0t00s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_da_ref_1t00s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_ref_1t00s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_da_ref_1t01s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_ref_1t01s/checkpoints/epoch\=4*"
 sbatch_gpu_short "eval_da_ref_1t10s_da" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_ref_1t10s/checkpoints/epoch\=4*"
-sbatch_gpu_short "eval_da_ref_1t00s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_ref_1t00s/checkpoints/epoch\=4*"
-sbatch_gpu_short "eval_da_ref_1t01s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_ref_1t01s/checkpoints/epoch\=4*"
-sbatch_gpu_short "eval_da_ref_1t10s_da_sim" "python3 experiments/13-eval_da_test.py --sim lightning_logs/multicand_ref_1t10s/checkpoints/epoch\=4*"
+sbatch_gpu_short "eval_da_ref_1t00s_da_sim" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_ref_1t00s/checkpoints/epoch\=4* --data data/csv/test_multi_sim.csv" 
+sbatch_gpu_short "eval_da_ref_1t01s_da_sim" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_ref_1t01s/checkpoints/epoch\=4* --data data/csv/test_multi_sim.csv" 
+sbatch_gpu_short "eval_da_ref_1t10s_da_sim" "python3 experiments/13-eval_da_test.py lightning_logs/multicand_ref_1t10s/checkpoints/epoch\=4* --data data/csv/test_multi_sim.csv" 
+
+# retrieval
+sbatch_gpu_short "eval_model_retrieval_1t_src" "python3 experiments/13-eval_da_test.py lightning_logs/model_retrieval_1t_src/checkpoints/epoch\=4* --data/csv/test_retrieval_src"
+sbatch_gpu_short "eval_model_retrieval_1t_mt"  "python3 experiments/13-eval_da_test.py lightning_logs/model_retrieval_1t_mt/checkpoints/epoch\=4*  --data/csv/test_retrieval_mt"
--- experiments/13-eval_da_test.py
+++ experiments/13-eval_da_test.py
@@ -4,15 +4,12 @@ import csv
 import utils
 
 args = argparse.ArgumentParser()
-args.add_argument("--sim", action="store_true")
 args.add_argument("model")
+args.add_argument("--data", default="data/csv/test_multi.csv")
 args = args.parse_args()
 
 model = comet_multi_cand.load_from_checkpoint(args.model)
-if args.sim:
-    data = list(csv.DictReader(open("data/csv/test_multi_sim.csv")))
-else:
-    data = list(csv.DictReader(open("data/csv/test_multi.csv")))
+data = list(csv.DictReader(open(args.data)))
 scores_pred = model.predict(data, batch_size=64).scores
 # assume the output is always a list the size of number of additional_score_out+1
 scores_pred = [x[0] for x in scores_pred]
