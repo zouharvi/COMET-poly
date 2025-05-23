c1106b925d2b7e15122aee516c736ff5c29ddbbb
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-05-23 13:42:39 +0200

    add support for other embedding types to retrieval data

--- experiments/01-get_data_retrieval.py
+++ experiments/01-get_data_retrieval.py
@@ -1,37 +1,54 @@
 import numpy as np
-import sentence_transformers.util
-import torch
 import tqdm
 import utils
 import csv
 import random
 import os
-import sentence_transformers
 import argparse
 from annoy import AnnoyIndex
 
 args = argparse.ArgumentParser()
 args.add_argument("--embd-key", default="mt", choices=["mt", "src", "srcAmt", "srcCmt"])
+args.add_argument("--embd-model", default="minilm", choices=["minilm", "xlmr", "comet"])
+args.add_argument("--max-sim", choices=["11", "09", "08", "07", "06"], default="11")
 args = args.parse_args()
 
-model = sentence_transformers.SentenceTransformer("all-MiniLM-L12-v2")
+max_sim = {
+    # no similarity filtering because max is 1.0
+    "11": 1.1,
+    "09": 0.9,
+    "08": 0.8,
+    "07": 0.7,
+    "06": 0.6,
+}[args.max_sim]
 
-def process_data(data, data_retrieval, data_retrieval_index: AnnoyIndex):
+R_FALLBACK = random.Random(0)
+
+def process_data(data, data_retrieval, data_retrieval_index: AnnoyIndex, prevent_hardmatch=False):
     print("Finding nearest neighbors")
     for line in tqdm.tqdm(data):
-        # we must select much more (n) because we are filtering it later on
-        idx, dists = data_retrieval_index.get_nns_by_vector(line["embd"], n=500, include_distances=True)
+        # select increasingly large neighbours because we filter later on
+        for n in [50, 100, 500, 1000, 5000]:
+            idx, dists = data_retrieval_index.get_nns_by_vector(line["embd"], n=n, include_distances=True)
+            idx = [i for i, d in zip(idx, dists) if d <= max_sim][:5]
+            tgts = [
+                (data_retrieval[i]["src"], data_retrieval[i]["mt"], data_retrieval[i]["score"])
+                for i in idx
+            ]
+            if prevent_hardmatch:
+                tgts = [
+                    x for x in tgts
+                    if (x[0], x[1]) != (line["src"], line["mt"])
+                ]
+            if len(tgts) == 5:
+                break
+        else:
+            # just take random examples at this point
+            tgts += [
+                (line["src"], line["mt"], line["score"])
+                for line in R_FALLBACK.sample(data_retrieval, 5-len(tgts))
+            ]
 
-        tgts = [
-            (data_retrieval[i]["src"], data_retrieval[i]["mt"], data_retrieval[i]["score"])
-            for i in idx
-        ]
-        # filter by hard match instead of inner product
-        # idx = [i for i, d in zip(idx, dists) if d < 0.999999][:5]
-        tgts = [
-            x for x in tgts
-            if (x[0], x[1]) != (line["src"], line["mt"])
-        ][:5]
         line["src2"], line["mt2"], line["score2"] = tgts[0]
         line["src3"], line["mt3"], line["score3"] = tgts[1]
         line["src4"], line["mt4"], line["score4"] = tgts[2]
@@ -40,49 +57,61 @@ def process_data(data, data_retrieval, data_retrieval_index: AnnoyIndex):
     
     return data
 
+def encode_txts(txts):
+
+    if args.embd_model in {"xlmr", "minilm"}:
+        import sentence_transformers
+        if args.embd_model == "xlmr":
+            model = sentence_transformers.SentenceTransformer("stsb-xlm-r-multilingual")
+        elif args.embd_model == "minilm":
+            model = sentence_transformers.SentenceTransformer("all-MiniLM-L12-v2")
+        
+        return {
+            txt: txt_e
+            for txt, txt_e in zip(txts, model.encode(txts, show_progress_bar=True, batch_size=256))
+        }
+    elif args.embd_model == "comet":
+        import comet
+        model = comet.load_from_checkpoint(comet.download_model("Unbabel/wmt22-comet-da")).to("cuda:0")
+        samples = {}
+        for txts_batch in tqdm.tqdm([txts[i:i+128] for i in range(0, len(txts), 128)]):
+            ids = model.encoder.prepare_sample(txts_batch).to("cuda:0")
+            ids = {
+                k: v.to("cuda:0")
+                for k, v in ids.items()
+            }
+            samples |= {
+                txt: txt_e
+                for txt, txt_e in zip(txts_batch, model.encoder(**ids)["sentemb"].cpu().numpy())
+            }
+        return samples
+
+
 def add_embd(data):
     print("Computing embeddings")
 
     if args.embd_key == "mt":
         tgt_all = list({line["mt"] for line in data})
-        tgt_to_embd = {
-            tgt: tgt_e
-            for tgt, tgt_e in zip(tgt_all, model.encode(tgt_all, show_progress_bar=True, batch_size=256, normalize_embeddings=True))
-        }
+        tgt_to_embd = encode_txts(tgt_all)
         for line in data:
             line["embd"] = tgt_to_embd[line["mt"]]
     elif args.embd_key == "src":
         src_all = list({line["src"] for line in data})
-        src_to_embd = {
-            tgt: tgt_e
-            for tgt, tgt_e in zip(src_all, model.encode(src_all, show_progress_bar=True, batch_size=256, normalize_embeddings=True))
-        }
+        src_to_embd = encode_txts(src_all)
         for line in data:
             line["embd"] = src_to_embd[line["src"]]
     elif args.embd_key == "srcAmt":
         src_all = list({line["src"] for line in data})
         tgt_all = list({line["mt"] for line in data})
-        tgt_to_embd = {
-            tgt: tgt_e
-            for tgt, tgt_e in zip(tgt_all, model.encode(tgt_all, show_progress_bar=True, batch_size=256))
-        }
-        src_to_embd = {
-            tgt: tgt_e
-            for tgt, tgt_e in zip(src_all, model.encode(src_all, show_progress_bar=True, batch_size=256))
-        }
+        tgt_to_embd = encode_txts(tgt_all)
+        src_to_embd = encode_txts(src_all)
         for line in data:
             line["embd"] = src_to_embd[line["src"]] + tgt_to_embd[line["mt"]]
     elif args.embd_key == "srcCmt":
         src_all = list({line["src"] for line in data})
         tgt_all = list({line["mt"] for line in data})
-        tgt_to_embd = {
-            tgt: tgt_e
-            for tgt, tgt_e in zip(tgt_all, model.encode(tgt_all, show_progress_bar=True, batch_size=256))
-        }
-        src_to_embd = {
-            tgt: tgt_e
-            for tgt, tgt_e in zip(src_all, model.encode(src_all, show_progress_bar=True, batch_size=256))
-        }
+        tgt_to_embd = encode_txts(tgt_all)
+        src_to_embd = encode_txts(src_all)
         for line in data:
             line["embd"] = np.concatenate((src_to_embd[line["src"]], tgt_to_embd[line["mt"]]))
     else:
@@ -121,10 +150,11 @@ data_train_index = AnnoyIndex(len(data_train[0]["embd"]), 'dot')
 for i, line in enumerate(data_train):
     data_train_index.add_item(i, line["embd"])
 print("Buliding Annoy")
-data_train_index.build(20)
+data_train_index.build(50)
 
-data_train = process_data(data_train, data_train, data_train_index)
-data_test = process_data(data_test, data_train, data_train_index)
+data_train = process_data(data_train, data_train, data_train_index, prevent_hardmatch=True)
+# for test, we don't mind at all if we retrieve the same translation
+data_test = process_data(data_test, data_train, data_train_index, prevent_hardmatch=False)
 
 # use 1k samples for dev
 data_dev_i = random.Random(0).sample(list(range(len(data_train))), k=1_000)
@@ -135,7 +165,7 @@ if __name__ == "__main__":
     os.makedirs("data/csv", exist_ok=True)
     def write_data(data, split):
         print("Writing", split, "of size", str(len(data)//1000)+"k")
-        with open(f"data/csv/{split}_retrieval_{args.embd_key}.csv", "w") as f:
+        with open(f"data/csv/{split}_retrieval_{args.embd_model}_{args.max_sim}_{args.embd_key}.csv", "w") as f:
             writer = csv.DictWriter(
                 f, fieldnames=[
                     "langs",
@@ -160,7 +190,15 @@ if __name__ == "__main__":
     write_data(data_dev, "dev")
 
 """
-function sbatch_gpu_short() {
+python3 experiments/01-get_data_retrieval.py --embd-key src
+python3 experiments/01-get_data_retrieval.py --embd-key srcAmt
+python3 experiments/01-get_data_retrieval.py --embd-key srcCmt
+python3 experiments/01-get_data_retrieval.py --embd-key mt
+python3 experiments/01-get_data_retrieval.py --embd-key srcCmt --embd-model comet --max-sim 07
+python3 experiments/01-get_data_retrieval.py --embd-model xlmr
+
+
+function sbatch_gpu() {
     JOB_NAME=$1;
     JOB_WRAP=$2;
     mkdir -p logs
@@ -172,13 +210,20 @@ function sbatch_gpu_short() {
         --mail-user vilem.zouhar@gmail.com \
         --ntasks-per-node=1 \
         --cpus-per-task=12 \
-        --mem-per-cpu=14G --time=0-4 \
+        --mem-per-cpu=14G --time=1-0 \
         --wrap="$JOB_WRAP";
 }
 
+for EMBDKEY in "src" "srcAmt" "srcCmt" "mt"; do
+for EMBDMODEL in "minilm" "xlmr" "comet"; do
+    MAXSIM="11"
+    sbatch_gpu "get_data_retrieval_${EMBDMODEL}_${MAXSIM}_${EMBDKEY}" "python3 experiments/01-get_data_retrieval.py --embd-key $EMBDKEY --embd-model $EMBDMODEL --max-sim $MAXSIM"
+done;
+done;
 
-sbatch_gpu_short "get_data_retrieval_src" "python3 experiments/01-get_data_retrieval.py --embd-key src"
-sbatch_gpu_short "get_data_retrieval_srcAmt" "python3 experiments/01-get_data_retrieval.py --embd-key srcAmt"
-sbatch_gpu_short "get_data_retrieval_srcCmt" "python3 experiments/01-get_data_retrieval.py --embd-key srcCmt"
-sbatch_gpu_short "get_data_retrieval_mt" "python3 experiments/01-get_data_retrieval.py --embd-key mt"
+for MAXSIM in "11" "09" "08" "07" "06"; do
+    EMBDMODEl="TODO"
+    EMBDKEY="TODO"
+    sbatch_gpu "get_data_retrieval_${EMBDMODEL}_${MAXSIM}_${EMBDKEY}" "python3 experiments/01-get_data_retrieval.py --embd-key $EMBDKEY --embd-model $EMBDMODEL --max-sim $MAXSIM"
+done;
 """
