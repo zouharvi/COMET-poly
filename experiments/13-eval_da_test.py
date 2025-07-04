0e6b358bb468824efc3b5bf194e642bb10a119e1
Maike Züfle <maike.zuefle@kit.edu>
2025-07-04 18:04:28 +0200

    Add lemontart

--- experiments/13-eval_da_test.py
+++ experiments/13-eval_da_test.py
@@ -2,16 +2,34 @@ import comet_multi_cand
 import argparse
 import csv
 import utils
+import os
 
 args = argparse.ArgumentParser()
 args.add_argument("model")
 args.add_argument("--data", default="data/csv/test_same_rand.csv")
+args.add_argument("--output_hyp", action="store_true")
+
 args = args.parse_args()
 
 model = comet_multi_cand.load_from_checkpoint(args.model)
 data = list(csv.DictReader(open(args.data)))
 scores_pred = model.predict(data, batch_size=64).scores
-# assume the output is always a list the size of number of additional_score_out+1
+
+if args.output_hyp:
+    # Extract model name by removing 'lightning_logs/' and everything after 'checkpoints/'
+    model_name = args.model.split("/checkpoints")[0]
+    data_name = args.data.split(".csv")[0].replace("/", "_")
+    output_filename = f"{model_name}/hyps_{data_name}.csv"
+
+    with open(output_filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["score"])
+        for x in scores_pred:
+            writer.writerow([x])
+
+    print("Hypthoeses stored in ", output_filename)
+
+# Now reduce scores_pred for eval
 scores_pred = [x[0] for x in scores_pred]
 
 res_pearson, res_kendall, res_errmean = utils.eval_da_per_lang(scores_pred, data)
